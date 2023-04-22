ccb1a4c05d924b3493f0f4e8c23dbeb6f0fbd2a7
dizzydroid <shehabmahmoud2003@gmail.com>
2023-04-22 11:46:19 +0200

    fixes

--- weather_app.py
+++ weather_app.py
@@ -4,8 +4,8 @@ import requests
 def get_weather_data(city):
 
     # Make an HTTP request to the OpenWeatherMap API.
+    def get_weather_data(city): response = requests.get("https://api.openweathermap.org/data/2.5/weather?q={}&appid=4a2205bea450247632b8fa33bae74868&units=metric".format(city))
     response = requests.get("https://api.openweathermap.org/data/2.5/weather?q={}&appid=4a2205bea450247632b8fa33bae74868&units=metric".format(city))
-
 
     # Check the response status code.
     if response.status_code == 200:
@@ -22,6 +22,20 @@ def get_weather_data(city):
         raise Exception("Request failed with status code {}".format(response.status_code))
 
 
+
+def display_weather_data(data):
+
+    # Print the current temperature.
+    print("Current temperature: {} degrees Celsius".format(data["main"]["temp"]))
+
+    # Print the humidity.
+    print("Humidity: {}%".format(data["main"]["humidity"]))
+
+    # Print the wind speed.
+    print("Wind speed: {} meters per second".format(data["wind"]["speed"]))
+
+
+
 def main():
 
     # Get the city from the user.
@@ -36,17 +50,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-def display_weather_data(data):
-
-    # Print the current temperature.
-    print("Current temperature: {} degrees Celsius".format(data["main"]["temp"]))
-
-    # Print the humidity.
-    print("Humidity: {}%".format(data["main"]["humidity"]))
-
-    # Print the wind speed.
-    print("Wind speed: {} meters per second".format(data["wind"]["speed"]))
 

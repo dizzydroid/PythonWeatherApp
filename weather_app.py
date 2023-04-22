a7630a2f4d635516d75da6af74d17fa1ce47183e
dizzydroid <shehabmahmoud2003@gmail.com>
2023-04-22 11:29:35 +0200

    initial commits

---
+++ weather_app.py
@@ -0,0 +1,52 @@
+import requests
+
+
+def get_weather_data(city):
+
+    # Make an HTTP request to the OpenWeatherMap API.
+    response = requests.get("https://api.openweathermap.org/data/2.5/weather?q={}&appid=4a2205bea450247632b8fa33bae74868&units=metric".format(city))
+
+
+    # Check the response status code.
+    if response.status_code == 200:
+
+        # The request was successful.
+        data = response.json()
+
+        # Return the weather data.
+        return data
+
+    else:
+
+        # The request failed.
+        raise Exception("Request failed with status code {}".format(response.status_code))
+
+
+def main():
+
+    # Get the city from the user.
+    city = input("Enter a city name: ")
+
+    # Get the weather data for the city.
+    data = get_weather_data(city)
+
+    # Display the weather data.
+    display_weather_data(data)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+def display_weather_data(data):
+
+    # Print the current temperature.
+    print("Current temperature: {} degrees Celsius".format(data["main"]["temp"]))
+
+    # Print the humidity.
+    print("Humidity: {}%".format(data["main"]["humidity"]))
+
+    # Print the wind speed.
+    print("Wind speed: {} meters per second".format(data["wind"]["speed"]))
+
